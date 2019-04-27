848ab097c4ee36c66a11bcbd7bb0460f6d7ebdf2
Jorge <julianacero98@gmail.com>
2019-04-27 17:06:39 -0500

    data sended to the state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import img from './cryptomonedas.png';
+import Form from './components/Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <img src={ img } alt="crypto" className="logotipo"/>
+          </div>
+          <div className="one-half column">
+            <h1>Quotes Cryptocurrencies Instantly</h1>
+            <Form/>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
